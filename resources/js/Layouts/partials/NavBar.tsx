b6e8e1bf2bd01de2e07b2b2a36887d38b0b0427c
oswin-jerome <oswinjeromej@gmail.com>
2023-12-26 22:27:08 +0530

    Typescript

--- resources/js/Layouts/partials/NavBar.tsx
+++ resources/js/Layouts/partials/NavBar.tsx
@@ -114,7 +114,7 @@ const NavBar = () => {
                 <ul className="flex flex-col ">
                     {menus.map((menu, index) => {
                         return (
-                            <li className="">
+                            <li className="" key={index}>
                                 <Link href={menu.to}>
                                     <div
                                         onClick={(e) => {
